49038e5df4b196b3d6b7f61dbfd09630522bf9a2
Nianyi Chen <nianyi.chen7@gmail.com>
2020-12-03 18:31:44 -0500

    debug drag

--- libgadget/blackhole.c
+++ libgadget/blackhole.c
@@ -827,7 +827,7 @@ blackhole_accretion_postprocess(int i, TreeWalk * tw)
     double rho_proper = rho * All.cf.a3inv;
 
     double soundspeed = blackhole_soundspeed(BH_GET_PRIV(tw)->BH_Entropy[PI], rho);
-    printf("soundspeed=%f \n", soundspeed);
+    printf("soundspeed=%f, entropy=%f, rho=%f \n", soundspeed,BH_GET_PRIV(tw)->BH_Entropy[PI],rho);
 
     /* Note: we take here a radiative efficiency of 0.1 for Eddington accretion */
     double meddington = (4 * M_PI * GRAVITY * LIGHTCGS * PROTONMASS / (0.1 * LIGHTCGS * LIGHTCGS * THOMPSON)) * BHP(i).Mass
